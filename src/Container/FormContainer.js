41399ef74c884741c9c27012878d155804ca8b6a
ing-arriola <ingeniero.arriola@gmail.com>
2021-01-02 16:43:40 -0600

    added form with bootstrap

--- src/Container/FormContainer.js
+++ src/Container/FormContainer.js
@@ -1,19 +1,51 @@
-import React from "react";
-import { Form, Field, ErrorMessage } from "formik";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
-const FormContainer = ({ isSubmitting }) => {
+const FormContainer = () => {
   return (
-    <Form>
-      <Field type="text" name="name" />
-      <Field type="text" name="lastname" />
-      <Field type="text" name="age" />
-      <Field type="email" name="email" />
-      <ErrorMessage name="email" component="div" />
-      <Field type="password" name="password" />
-      <ErrorMessage name="password" component="div" />
-      <button type="submit" disabled={isSubmitting}>
+    <Form className="mx-5 px-5 ">
+      <Form.Group controlId="formName">
+        <Form.Label>Nombres</Form.Label>
+        <Form.Control type="text" placeholder="Escribe tus nombres" />
+      </Form.Group>
+      <Form.Group controlId="formLastname">
+        <Form.Label>Apellidos</Form.Label>
+        <Form.Control type="text" placeholder="Escribe tus apellidos" />
+      </Form.Group>
+      <Form.Group controlId="formage">
+        <Form.Label>Edad</Form.Label>
+        <Form.Control type="text" placeholder="Ingresa tu edad" />
+      </Form.Group>
+      <Form.Group controlId="formPhone">
+        <Form.Label>Telefono</Form.Label>
+        <Form.Control type="text" placeholder="Telefono principal" />
+      </Form.Group>
+      <Form.Group controlId="formPhone2">
+        <Form.Label></Form.Label>
+        <Form.Control type="text" placeholder="Telefono secundario" />
+      </Form.Group>
+      <Form.Group controlId="formEmail">
+        <Form.Label></Form.Label>
+        <Form.Control type="email" placeholder="correo electronico" />
+      </Form.Group>
+
+      <Form.Group controlId="formEducation">
+        <Form.Label>Nivel de estudios</Form.Label>
+        <Form.Control as="select">
+          <option>Bachillerato</option>
+          <option>Universidad (No graduado) </option>
+          <option>Universidad (Gradudado)</option>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
         Submit
-      </button>
+      </Button>
     </Form>
   );
 };
