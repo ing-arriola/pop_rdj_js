b76ec06f0a3da9538c26e27e5740b6afe6a99b7f
RauPro <00012119@uca.edu.sv>
2021-01-04 19:34:31 -0600

    Added 2 more sections and footer

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,7 +2,11 @@ import React from "react";
 import NavbarContainer from "../Container/NavbarContainer";
 import {Button, Container} from "react-bootstrap";
 import imgLop from "../Resources/undraw_investment_data_yfvt.svg"
+import imtAbout from "../Resources/undraw_Business_decisions_re_84ag.svg"
+import imtOurMission from "../Resources/undraw_Social_ideas_re_j5v4.svg"
+
 import {Link} from "react-router-dom";
+import FooterContainer from "../Container/FooterContainer";
 const HomePage = () => {
   return (
       <>
@@ -16,6 +20,26 @@ const HomePage = () => {
               </div>
               <img src={imgLop} width={800}/>
           </Container>
+          <Container className="d-flex justify-content-around pt-5">
+                <div className="w-25">
+                    <h1>Sobre nosotros</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis iste nemo praesentium repellat. Assumenda, harum, veniam? Amet consequatur cumque cupiditate minus molestias quaerat voluptatum? Accusamus aut hic iste tempore temporibus!</p>
+                </div>
+                <div>
+                    <img src={imtAbout} width={450}/>
+                </div>
+          </Container>
+          <Container className="d-flex justify-content-around pt-5 pb-5">
+              <div>
+                  <img src={imtOurMission} width={450}/>
+              </div>
+              <div className="w-25">
+                  <h1>Nuestra mision</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aliquid consequuntur debitis dolore ducimus eaque eius exercitationem ipsam ipsum labore laudantium neque nisi omnis quam quia quidem quo, repudiandae?</p>
+              </div>
+
+          </Container>
+          <FooterContainer/>
       </div>
       </>
   );
