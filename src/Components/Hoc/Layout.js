c92e70335f1b69db9c4795823cba714e8ae342f7
RauPro <00012119@uca.edu.sv>
2021-01-20 10:00:37 -0600

    Added Aux

--- src/Components/Hoc/Layout.js
+++ src/Components/Hoc/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Aux from "./Aux";
+import Aux from "./_Aux";
 import NavbarContainer from "../../Container/NavbarContainer";
 import FooterContainer from "../../Container/FooterContainer";
 
