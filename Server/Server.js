a835131acaa3857b5f365e99caa4a92eb75e6674
RauPro <00012119@uca.edu.sv>
2021-01-21 10:34:33 -0600

    Added: Connection DB and Server

--- Server/Server.js
+++ Server/Server.js
@@ -5,7 +5,21 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express();
 app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
+
+
+// bd connect
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@pop.zsndr.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+    .then(() => console.log('DB Connected'))
+    .catch(e => console.log('error db:', e))
+
+const authRoutes = require('./controller/action');
+app.use('/api/user', authRoutes);
 // start the server in the port 3005 !
 app.listen(3000, function () {
     console.log('App listening on port 3005.');
