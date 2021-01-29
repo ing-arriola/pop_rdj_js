46eb7d82cd4b32c796ac03f9ea126ee1259e4d09
RauPro <00012119@uca.edu.sv>
2021-01-29 09:36:03 -0600

    Added: serverURL

--- client/src/configs/AuthServices.js
+++ client/src/configs/AuthServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3005/";
+const API_URL = "https://pasantias-pop.herokuapp.com/";
 class AuthServices{
     async register(userObject) {
          return await axios.post(API_URL + "api/user/register", userObject);
