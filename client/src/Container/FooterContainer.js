66f4f65026332e89400bb771e8bd1c30656bf4b1
RauPro <00012119@uca.edu.sv>
2021-01-29 10:03:03 -0600

    Added: footer

--- client/src/Container/FooterContainer.js
+++ client/src/Container/FooterContainer.js
@@ -5,7 +5,7 @@ import logo from "../Resources/logo_rjf.png";
 export default function FooterContainer() {
   return (
     <footer className="bg-light text-center text-lg-start">
-      <div className="container p-4">
+      <div className="container pt-4 pb-0">
         <div className="row">
           <div className="col-lg-3 col-md-12 mb-4 mb-md-0">
             <img
@@ -17,7 +17,7 @@ export default function FooterContainer() {
             />
           </div>
 
-          <div className="col-lg-6 col-md-12 mb-4 mb-md-0 ml-auto weight-300 ">
+          <div className="col-lg-6 col-md-12 mb-2 mb-md-0 ml-auto weight-300 ">
             <p>
               Somos una organización ciudadana juvenil sin fines de lucro que
               trabaja ideas alineadas a los Objetivos de Desarrollo Sostenible
@@ -47,8 +47,12 @@ export default function FooterContainer() {
           </div>
         </div>
       </div>
-      <div className="text-center p-3">
+      <div className="text-center mb-1">
         Red de jóvenes del futuro {new Date().getFullYear()} ©
+      </div>
+      <div style={{width: "100%", height: "50px", backgroundColor: "#c9c9c9"}}>
+          <h5 style={{lineHeight: "50px"}}> Website developed by: <a href="https://jaime-arriola.netlify.app/" target="_blank" style={{fontWeight: "bold" , color: "black"}}>Jaime Arriola</a> and <a href="https://raupro.herokuapp.com/" target="_blank" style={{fontWeight: "bold", color: "black"}}>Raul Guillen</a>
+          </h5>
       </div>
     </footer>
   );
