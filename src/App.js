bcf679fc6b9ce641ad8693938cd62e712a4c40fe
Jaime <ingeniero.arriola@gmail.com>
2021-04-15 10:03:57 -0600

    Added global CSS file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Layout from "./Components/Hoc/Layout";
-import HomePage from "./Pages/HomePage";
-import Register from "./Pages/Register";
-import Users from './Pages/Users'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Layout from "./Components/Hoc/Layout"
+import HomePage from "./Pages/HomePage"
+// import Register from "./Pages/Register"
+// import Users from './Pages/Users'
+import Survey from './Pages/Survey'
 import {Auth0Provider} from '@auth0/auth0-react'
+import "./App.css"
 
 const domain=process.env.REACT_APP_AUTH0_DOMAIN
 const id=process.env.REACT_APP_AUTH0_CLIENT_ID
@@ -21,6 +23,7 @@ function App() {
         <Switch>
           {/*<Route path="/users" component={Users}></Route>*/}
           {/*<Route path="/signup" component={Register}></Route>*/}
+           <Route path="/survey" component={Survey}></Route>
           <Route path="/" component={HomePage}></Route>
         </Switch>
       </Layout>
