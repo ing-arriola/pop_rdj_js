be55d74bd727b20c35eef19960f0950cd101a979
Raul Guillen <reguillen@applaudostudios.com>
2021-04-23 16:53:58 -0700

    Add: login route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Layout from "./Components/Hoc/Layout"
 import HomePage from "./Pages/HomePage"
 // import Register from "./Pages/Register"
-// import Users from './Pages/Users'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Users from './Pages/Users'
-import PrivateRoute from './Container/PrivateRoute'
-import {Auth0Provider} from '@auth0/auth0-react'
 import "./App.css"
 import Results from "./Pages/Results";
 import SingleResult from "./Pages/SingleResult";
-import {firebaseConfig} from "./firebase";
+import LoginPage from "./Pages/LoginPage";
 
 const domain=process.env.REACT_APP_AUTH0_DOMAIN
 const id=process.env.REACT_APP_AUTH0_CLIENT_ID
@@ -18,24 +15,17 @@ const id=process.env.REACT_APP_AUTH0_CLIENT_ID
 
 const App = () => {
     return (
-      <Auth0Provider
-        domain={domain}
-        clientId={id}
-        redirectUri={window.location.origin}
-      >
         <Router>
-        <Layout>
-          <Switch>
-            {/*<Route path="/users" component={Users}></Route>*/}
-            {/*<Route path="/signup" component={Register}></Route>*/}
-             <PrivateRoute path="/users" component={Users}/>
-              <PrivateRoute path="/results/:id" component={SingleResult}/>
-              <PrivateRoute path="/results" component={Results}/>
-            <Route path="/" component={HomePage}/>
-          </Switch>
-        </Layout>
-      </Router>
-      </Auth0Provider>
+            <Layout>
+                <Switch>
+                    <Route path="/login" component={LoginPage}/>
+                    <Route path="/users" component={Users}/>
+                    <Route path="/results/:id" component={SingleResult}/>
+                    <Route path="/results" component={Results}/>
+                    <Route path="/" component={HomePage}/>
+                </Switch>
+            </Layout>
+        </Router>
     )
 }
 
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const LoginPage = () => {
+    return (
+        <>
+            <div>
+                Login
+            </div>
+        </>
+    );
+};
+
+export default LoginPage;
