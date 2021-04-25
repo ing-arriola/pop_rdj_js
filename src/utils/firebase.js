4f6aa851d9f7615e7afc44c6430afeffb54abffc
RauPro <00012119@uca.edu.sv>
2021-04-24 20:21:10 -0700

    Change var to const

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase"
 import "firebase/auth"
 
-var firebaseConfig = firebase.initializeApp ({
+const firebaseConfig = firebase.initializeApp ({
     apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_URL,
