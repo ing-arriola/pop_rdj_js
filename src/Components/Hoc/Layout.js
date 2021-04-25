4c9a4d30b50869425540418ea5e2c3b0c31b2ba1
RauPro <00012119@uca.edu.sv>
2021-04-24 19:51:41 -0700

    Fix: footer

--- src/Components/Hoc/Layout.js
+++ src/Components/Hoc/Layout.js
@@ -6,12 +6,12 @@ import FooterContainer from "../../Container/FooterContainer";
 const Layout = (props) => {
     const location = window.location.pathname;
   return (
-    <div>
+    <div style={{position: "relative", paddingBottom: 270}}>
         <Aux>
             <NavbarContainer />
 
             <> {props.children} </>
-            <div className='footer' style={{position: location.includes('login') ? 'Fixed':'Static', bottom: 0, width: '100%'}}>
+            <div className='footer' style={{position: "absolute", bottom: 0, width: "100%"}}>
                 <FooterContainer />
             </div>
         </Aux>
